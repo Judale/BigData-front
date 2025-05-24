88fa53c116b51f2370579efb3de5e04092bbebc5
Alexis Macle <135204928+Alexismacle@users.noreply.github.com>
2025-05-24 16:57:58 +0200

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,15 +2,15 @@ import axios from "axios";
 import { getToken } from "../utils/auth";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:5000",
+  baseURL: "/api",  // Utilise le path relatif en production et dev
 });
 
 api.interceptors.request.use((config) => {
-    const token = getToken();
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
