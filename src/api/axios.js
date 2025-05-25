62d20459a7ad75465cbed21af2333dc80b8fe376
Alexis Macle <135204928+Alexismacle@users.noreply.github.com>
2025-05-25 11:24:11 +0200

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/auth";
 
 const api = axios.create({
-  baseURL: "",
+  baseURL: "/api",
 });
 
 api.interceptors.request.use((config) => {
